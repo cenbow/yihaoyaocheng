3fe4b51459d4230ffbf0c4e474afa65a13914ee0
liqiang <liqiang@111.com.cn>
2016-08-08 14:55:36 +0800

    退货订单新增退货说明字段

--- order-dao/src/main/java/com/yyw/yhyc/order/bo/OrderException.java
+++ order-dao/src/main/java/com/yyw/yhyc/order/bo/OrderException.java
@@ -1,9 +1,9 @@
 /**
  * Created By: XI
- * Created On: 2016-8-8 10:31:59
+ * Created On: 2016-8-8 14:53:12
  *
  * Amendment History:
- * 
+ *
  * Amended By       Amended On      Amendment Description
  * ------------     -----------     ---------------------------------------------
  *
@@ -15,415 +15,437 @@ import com.yyw.yhyc.bo.Model;
 public class OrderException extends Model{
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long	serialVersionUID	= 1L;
-	
+
 	/**
-	  *	
-	  */
+	 *
+	 */
 	private java.lang.Integer exceptionId;
 
 	/**
-	  *	1:退货
-  2:换货
-  3:补货 4: 拒收
-	  */
+	 *	1:退货
+	 2:换货
+	 3:补货 4: 拒收
+	 */
 	private java.lang.String returnType;
 
 	/**
-	  *	异常订单编码
-	  */
+	 *	异常订单编码
+	 */
 	private java.lang.String exceptionOrderId;
 
 	/**
-	  *	原订单ID主键
-	  */
+	 *	原订单ID主键
+	 */
 	private java.lang.Integer orderId;
 
 	/**
-	  *	原订单编号
-	  */
+	 *	原订单编号
+	 */
 	private java.lang.String flowId;
 
 	/**
-	  *	异常订单的状态 1 待确认 2 退款中 3 已完成 4 已关闭
-	  */
+	 *	异常订单的状态 1 待确认 2 退款中 3 已完成 4 已关闭
+	 */
 	private java.lang.String orderStatus;
 
 	/**
-	  *	采购商名称
-	  */
+	 *	采购商名称
+	 */
 	private java.lang.String custName;
 
 	/**
-	  *	客户ID
-	  */
+	 *	客户ID
+	 */
 	private java.lang.Integer custId;
 
 	/**
-	  *	供应商名称
-	  */
+	 *	供应商名称
+	 */
 	private java.lang.String supplyName;
 
 	/**
-	  *	供应商ID
-	  */
+	 *	供应商ID
+	 */
 	private java.lang.Integer supplyId;
 
 	/**
-	  *	备注
-	  */
+	 *	备注(确认/关闭说明)
+	 */
 	private java.lang.String remark;
 
 	/**
-	  *	原订单金额
-	  */
+	 *	原订单金额
+	 */
 	private java.math.BigDecimal orderMoneyTotal;
 
 	/**
-	  *	异常订单金额
-	  */
+	 *	异常订单金额
+	 */
 	private java.math.BigDecimal orderMoney;
 
 	/**
-	  *	
-	  */
+	 *
+	 */
 	private java.lang.String createUser;
 
 	/**
-	  *	
-	  */
+	 *
+	 */
 	private java.lang.String createTime;
 
 	/**
-	  *	
-	  */
+	 *
+	 */
 	private java.lang.String updateUser;
 
 	/**
-	  *	
-	  */
+	 *
+	 */
 	private java.lang.String updateTime;
 
 	/**
-	  *	订单生成时间
-	  */
+	 *	订单生成时间
+	 */
 	private java.lang.String orderCreateTime;
 
 	/**
-	  *	
-	  */
-	public java.lang.Integer getExceptionId() 
+	 *	退换货描述(拒收说明)
+	 */
+	private java.lang.String returnDesc;
+
+	/**
+	 *
+	 */
+	public java.lang.Integer getExceptionId()
 	{
 		return exceptionId;
 	}
-	
+
 	/**
-	  *	
-	  */
-	public void setExceptionId(java.lang.Integer exceptionId) 
+	 *
+	 */
+	public void setExceptionId(java.lang.Integer exceptionId)
 	{
 		this.exceptionId = exceptionId;
 	}
-	
+
 	/**
-	  *	1:退货
-  2:换货
-  3:补货 4: 拒收
-	  */
-	public java.lang.String getReturnType() 
+	 *	1:退货
+	 2:换货
+	 3:补货 4: 拒收
+	 */
+	public java.lang.String getReturnType()
 	{
 		return returnType;
 	}
-	
+
 	/**
-	  *	1:退货
-  2:换货
-  3:补货 4: 拒收
-	  */
-	public void setReturnType(java.lang.String returnType) 
+	 *	1:退货
+	 2:换货
+	 3:补货 4: 拒收
+	 */
+	public void setReturnType(java.lang.String returnType)
 	{
 		this.returnType = returnType;
 	}
-	
+
 	/**
-	  *	异常订单编码
-	  */
-	public java.lang.String getExceptionOrderId() 
+	 *	异常订单编码
+	 */
+	public java.lang.String getExceptionOrderId()
 	{
 		return exceptionOrderId;
 	}
-	
+
 	/**
-	  *	异常订单编码
-	  */
-	public void setExceptionOrderId(java.lang.String exceptionOrderId) 
+	 *	异常订单编码
+	 */
+	public void setExceptionOrderId(java.lang.String exceptionOrderId)
 	{
 		this.exceptionOrderId = exceptionOrderId;
 	}
-	
+
 	/**
-	  *	原订单ID主键
-	  */
-	public java.lang.Integer getOrderId() 
+	 *	原订单ID主键
+	 */
+	public java.lang.Integer getOrderId()
 	{
 		return orderId;
 	}
-	
+
 	/**
-	  *	原订单ID主键
-	  */
-	public void setOrderId(java.lang.Integer orderId) 
+	 *	原订单ID主键
+	 */
+	public void setOrderId(java.lang.Integer orderId)
 	{
 		this.orderId = orderId;
 	}
-	
+
 	/**
-	  *	原订单编号
-	  */
-	public java.lang.String getFlowId() 
+	 *	原订单编号
+	 */
+	public java.lang.String getFlowId()
 	{
 		return flowId;
 	}
-	
+
 	/**
-	  *	原订单编号
-	  */
-	public void setFlowId(java.lang.String flowId) 
+	 *	原订单编号
+	 */
+	public void setFlowId(java.lang.String flowId)
 	{
 		this.flowId = flowId;
 	}
-	
+
 	/**
-	  *	异常订单的状态 1 待确认 2 退款中 3 已完成 4 已关闭
-	  */
-	public java.lang.String getOrderStatus() 
+	 *	异常订单的状态 1 待确认 2 退款中 3 已完成 4 已关闭
+	 */
+	public java.lang.String getOrderStatus()
 	{
 		return orderStatus;
 	}
-	
+
 	/**
-	  *	异常订单的状态 1 待确认 2 退款中 3 已完成 4 已关闭
-	  */
-	public void setOrderStatus(java.lang.String orderStatus) 
+	 *	异常订单的状态 1 待确认 2 退款中 3 已完成 4 已关闭
+	 */
+	public void setOrderStatus(java.lang.String orderStatus)
 	{
 		this.orderStatus = orderStatus;
 	}
-	
+
 	/**
-	  *	采购商名称
-	  */
-	public java.lang.String getCustName() 
+	 *	采购商名称
+	 */
+	public java.lang.String getCustName()
 	{
 		return custName;
 	}
-	
+
 	/**
-	  *	采购商名称
-	  */
-	public void setCustName(java.lang.String custName) 
+	 *	采购商名称
+	 */
+	public void setCustName(java.lang.String custName)
 	{
 		this.custName = custName;
 	}
-	
+
 	/**
-	  *	客户ID
-	  */
-	public java.lang.Integer getCustId() 
+	 *	客户ID
+	 */
+	public java.lang.Integer getCustId()
 	{
 		return custId;
 	}
-	
+
 	/**
-	  *	客户ID
-	  */
-	public void setCustId(java.lang.Integer custId) 
+	 *	客户ID
+	 */
+	public void setCustId(java.lang.Integer custId)
 	{
 		this.custId = custId;
 	}
-	
+
 	/**
-	  *	供应商名称
-	  */
-	public java.lang.String getSupplyName() 
+	 *	供应商名称
+	 */
+	public java.lang.String getSupplyName()
 	{
 		return supplyName;
 	}
-	
+
 	/**
-	  *	供应商名称
-	  */
-	public void setSupplyName(java.lang.String supplyName) 
+	 *	供应商名称
+	 */
+	public void setSupplyName(java.lang.String supplyName)
 	{
 		this.supplyName = supplyName;
 	}
-	
+
 	/**
-	  *	供应商ID
-	  */
-	public java.lang.Integer getSupplyId() 
+	 *	供应商ID
+	 */
+	public java.lang.Integer getSupplyId()
 	{
 		return supplyId;
 	}
-	
+
 	/**
-	  *	供应商ID
-	  */
-	public void setSupplyId(java.lang.Integer supplyId) 
+	 *	供应商ID
+	 */
+	public void setSupplyId(java.lang.Integer supplyId)
 	{
 		this.supplyId = supplyId;
 	}
-	
+
 	/**
-	  *	备注
-	  */
-	public java.lang.String getRemark() 
+	 *	备注(确认/关闭说明)
+	 */
+	public java.lang.String getRemark()
 	{
 		return remark;
 	}
-	
+
 	/**
-	  *	备注
-	  */
-	public void setRemark(java.lang.String remark) 
+	 *	备注(确认/关闭说明)
+	 */
+	public void setRemark(java.lang.String remark)
 	{
 		this.remark = remark;
 	}
-	
+
 	/**
-	  *	原订单金额
-	  */
-	public java.math.BigDecimal getOrderMoneyTotal() 
+	 *	原订单金额
+	 */
+	public java.math.BigDecimal getOrderMoneyTotal()
 	{
 		return orderMoneyTotal;
 	}
-	
+
 	/**
-	  *	原订单金额
-	  */
-	public void setOrderMoneyTotal(java.math.BigDecimal orderMoneyTotal) 
+	 *	原订单金额
+	 */
+	public void setOrderMoneyTotal(java.math.BigDecimal orderMoneyTotal)
 	{
 		this.orderMoneyTotal = orderMoneyTotal;
 	}
-	
+
 	/**
-	  *	异常订单金额
-	  */
-	public java.math.BigDecimal getOrderMoney() 
+	 *	异常订单金额
+	 */
+	public java.math.BigDecimal getOrderMoney()
 	{
 		return orderMoney;
 	}
-	
+
 	/**
-	  *	异常订单金额
-	  */
-	public void setOrderMoney(java.math.BigDecimal orderMoney) 
+	 *	异常订单金额
+	 */
+	public void setOrderMoney(java.math.BigDecimal orderMoney)
 	{
 		this.orderMoney = orderMoney;
 	}
-	
+
 	/**
-	  *	
-	  */
-	public java.lang.String getCreateUser() 
+	 *
+	 */
+	public java.lang.String getCreateUser()
 	{
 		return createUser;
 	}
-	
+
 	/**
-	  *	
-	  */
-	public void setCreateUser(java.lang.String createUser) 
+	 *
+	 */
+	public void setCreateUser(java.lang.String createUser)
 	{
 		this.createUser = createUser;
 	}
-	
+
 	/**
-	  *	
-	  */
-	public java.lang.String getCreateTime() 
+	 *
+	 */
+	public java.lang.String getCreateTime()
 	{
 		return createTime;
 	}
-	
+
 	/**
-	  *	
-	  */
-	public void setCreateTime(java.lang.String createTime) 
+	 *
+	 */
+	public void setCreateTime(java.lang.String createTime)
 	{
 		this.createTime = createTime;
 	}
-	
+
 	/**
-	  *	
-	  */
-	public java.lang.String getUpdateUser() 
+	 *
+	 */
+	public java.lang.String getUpdateUser()
 	{
 		return updateUser;
 	}
-	
+
 	/**
-	  *	
-	  */
-	public void setUpdateUser(java.lang.String updateUser) 
+	 *
+	 */
+	public void setUpdateUser(java.lang.String updateUser)
 	{
 		this.updateUser = updateUser;
 	}
-	
+
 	/**
-	  *	
-	  */
-	public java.lang.String getUpdateTime() 
+	 *
+	 */
+	public java.lang.String getUpdateTime()
 	{
 		return updateTime;
 	}
-	
+
 	/**
-	  *	
-	  */
-	public void setUpdateTime(java.lang.String updateTime) 
+	 *
+	 */
+	public void setUpdateTime(java.lang.String updateTime)
 	{
 		this.updateTime = updateTime;
 	}
-	
+
 	/**
-	  *	订单生成时间
-	  */
-	public java.lang.String getOrderCreateTime() 
+	 *	订单生成时间
+	 */
+	public java.lang.String getOrderCreateTime()
 	{
 		return orderCreateTime;
 	}
-	
+
 	/**
-	  *	订单生成时间
-	  */
-	public void setOrderCreateTime(java.lang.String orderCreateTime) 
+	 *	订单生成时间
+	 */
+	public void setOrderCreateTime(java.lang.String orderCreateTime)
 	{
 		this.orderCreateTime = orderCreateTime;
 	}
-	
+
+	/**
+	 *	退换货描述(拒收说明)
+	 */
+	public java.lang.String getReturnDesc()
+	{
+		return returnDesc;
+	}
+
+	/**
+	 *	退换货描述(拒收说明)
+	 */
+	public void setReturnDesc(java.lang.String returnDesc)
+	{
+		this.returnDesc = returnDesc;
+	}
+
 	public String toString()
 	{
-		return "OrderException [" + 
-					"exceptionId=" + exceptionId + 
-					", returnType=" + returnType + 
-					", exceptionOrderId=" + exceptionOrderId + 
-					", orderId=" + orderId + 
-					", flowId=" + flowId + 
-					", orderStatus=" + orderStatus + 
-					", custName=" + custName + 
-					", custId=" + custId + 
-					", supplyName=" + supplyName + 
-					", supplyId=" + supplyId + 
-					", remark=" + remark + 
-					", orderMoneyTotal=" + orderMoneyTotal + 
-					", orderMoney=" + orderMoney + 
-					", createUser=" + createUser + 
-					", createTime=" + createTime + 
-					", updateUser=" + updateUser + 
-					", updateTime=" + updateTime + 
-					", orderCreateTime=" + orderCreateTime + 
+		return "OrderException [" +
+				"exceptionId=" + exceptionId +
+				", returnType=" + returnType +
+				", exceptionOrderId=" + exceptionOrderId +
+				", orderId=" + orderId +
+				", flowId=" + flowId +
+				", orderStatus=" + orderStatus +
+				", custName=" + custName +
+				", custId=" + custId +
+				", supplyName=" + supplyName +
+				", supplyId=" + supplyId +
+				", remark=" + remark +
+				", orderMoneyTotal=" + orderMoneyTotal +
+				", orderMoney=" + orderMoney +
+				", createUser=" + createUser +
+				", createTime=" + createTime +
+				", updateUser=" + updateUser +
+				", updateTime=" + updateTime +
+				", orderCreateTime=" + orderCreateTime +
+				", returnDesc=" + returnDesc +
 				"]";
 	}
 }
