046b85d25cc49322c37915524fe21108ba6f8875
liuyang <liuyang@111.com.cn>
2016-08-22 18:18:44 +0800

    退货、换货调用武汉账期接口

--- order-service/src/main/java/com/yyw/yhyc/order/service/OrderExceptionService.java
+++ order-service/src/main/java/com/yyw/yhyc/order/service/OrderExceptionService.java
@@ -1715,4 +1715,13 @@ public class OrderExceptionService {
 		}
 	}
 
+	/**
+	 * 根据订单编码，获取退货和拒收订单金额历史总额
+	 * @param flowId
+	 * @return
+	 */
+	public BigDecimal getConfirmHistoryExceptionMoney(String flowId){
+		BigDecimal  historyMoneys = orderExceptionMapper.getConfirmHistoryExceptionMoney(flowId);
+		return historyMoneys;
+	}
 }
