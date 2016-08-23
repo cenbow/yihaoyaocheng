b1fe3b8251eb12347253b463f93a42bdf0953f4d
liuhaikuo <@111.com.cn>
2016-08-23 11:17:47 +0800

    接收也可以退款结算

--- order-web/src/main/webapp/static/js/order/order_settlement_seller.js
+++ order-web/src/main/webapp/static/js/order/order_settlement_seller.js
@@ -164,7 +164,7 @@ function fillTableJson(data) {
 //类型 转换成操作
 function typeToOperate(businessType,confirm,settlementId) {
 	var result = '';
-	if(businessType==2){//只有退款有操作
+	if(businessType==2||businessType==3){//只有退款有操作
 		if(confirm ==0){
 			result = '<button type="button" class="btn btn-info btn-sm editbtn back-opreate" data-stmid = "'+settlementId+'">退款结算</button>';
 		}else  if(confirm ==1){
