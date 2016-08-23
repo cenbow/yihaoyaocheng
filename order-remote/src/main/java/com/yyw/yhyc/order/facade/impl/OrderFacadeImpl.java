fa961d3892d998389c24c5f4f041afc5a9eafbce
lvhongjie <lvhongjie@111.com.cn>
2016-08-23 15:13:06 +0800

    去掉 facede 里创建订单的调用

--- order-remote/src/main/java/com/yyw/yhyc/order/facade/impl/OrderFacadeImpl.java
+++ order-remote/src/main/java/com/yyw/yhyc/order/facade/impl/OrderFacadeImpl.java
@@ -150,7 +150,7 @@ public class OrderFacadeImpl implements OrderFacade {
 	 * @throws Exception
 	 */
 	public List<Order> createOrder(OrderCreateDto orderCreateDto) throws Exception {
-		return orderService.createOrder(orderCreateDto);
+		return null;
 	}
 
 	/**
