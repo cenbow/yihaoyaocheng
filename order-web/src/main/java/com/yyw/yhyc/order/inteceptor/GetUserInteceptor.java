67092db7dc369e9efa7375d8653b8f53bf0023c7
shiyongxi <shiyongxi@111.com.cn>
2016-08-31 14:13:26 +0800

    获取登录时存储的常用信息
    Change-Id: I44370d6c2ab313374a15225d6d29755f09871caf

--- order-web/src/main/java/com/yyw/yhyc/order/inteceptor/GetUserInteceptor.java
+++ order-web/src/main/java/com/yyw/yhyc/order/inteceptor/GetUserInteceptor.java
@@ -32,16 +32,33 @@ public class GetUserInteceptor extends HandlerInterceptorAdapter {
             User u = (User) request.getAttribute("loginUser");
             if(!UtilHelper.isEmpty(u) && UtilHelper.isEmpty(request.getAttribute(UserDto.REQUEST_KEY))){
                 String commonInfo = CacheUtil.getSingleton().get(CACHE_PREFIX + COMMON_INFO + request.getSession().getId());
+                String user = CacheUtil.getSingleton().get("passport" + request.getSession().getId());
                 UserDto userDto = new UserDto();
                 userDto.setUserName(u.getUsername());
                 userDto.setCustId(u.getEnterprise_id());
 
+                //用户信息
+                if(!UtilHelper.isEmpty(user))
+                {
+                    Map<String, Object> map = JSONObject.parseObject(user, HashMap.class);
+                    userDto.setUser(map);
+                }
+
+                //企业信息
                 if(!UtilHelper.isEmpty(commonInfo)){
                     Map<String, String> map = JSONObject.parseObject(commonInfo, HashMap.class);
 
                     log.info("commonInfo-->" + commonInfo);
 
                     userDto.setCustName(map.get("enterpriseName"));
+                    userDto.setProvince(map.get("province"));
+                    userDto.setProvinceName(map.get("provinceName"));
+                    userDto.setCity(map.get("city"));
+                    userDto.setCityName(map.get("cityName"));
+                    userDto.setDistrict(map.get("district"));
+                    userDto.setDistrictName(map.get("districtName"));
+                    userDto.setRegisteredAddress(map.get("registeredAddress"));
+
                     String roleType = map.get("roleType");
                     if(!UtilHelper.isEmpty(roleType)){
                         CustTypeEnum custTypeEnum = null;
