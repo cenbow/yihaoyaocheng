944488787987ae38b494bdf58c39124e8dc21794
zhangqiang <936669533@qq.com>
2016-09-02 14:54:04 +0800

    订单退款逻辑修改

--- order-service/src/main/java/com/yyw/yhyc/pay/impl/ChinaPayServiceImpl.java
+++ order-service/src/main/java/com/yyw/yhyc/pay/impl/ChinaPayServiceImpl.java
@@ -587,6 +587,18 @@ public class ChinaPayServiceImpl implements PayService {
 
         OrderPay orderPay =  orderPayMapper.getByPayFlowId(orderCombined.getPayFlowId());
 
+        String now = systemDateMapper.getSystemDate();
+        orderRefund.setCreateUser(userDto.getUserName());
+        orderRefund.setCustId(order.getCustId());
+        orderRefund.setSupplyId(order.getSupplyId());
+        orderRefund.setRefundSum(order.getOrgTotal());
+        orderRefund.setRefundFreight(new BigDecimal(0));
+        orderRefund.setFlowId(flowId);
+        orderRefund.setCreateTime(now);
+        orderRefund.setRefundDate(now);
+        orderRefund.setRefundStatus(SystemRefundPayStatusEnum.refundStatusIng.getType());//退款中
+        orderRefund.setRefundDesc(refundDesc);
+
         List<Order> orderList = new ArrayList<Order>();
         orderList.add(order);
         Map<String, String> resultMap = null;
@@ -601,17 +613,6 @@ public class ChinaPayServiceImpl implements PayService {
             log.error("调用银联退款，调用银联退款接口失败，"+resultMap.get("msg"));
             throw new RuntimeException("调用银联退款接口失败，"+resultMap.get("msg"));
         }
-        String now = systemDateMapper.getSystemDate();
-        orderRefund.setCreateUser(userDto.getUserName());
-        orderRefund.setCustId(order.getCustId());
-        orderRefund.setSupplyId(order.getSupplyId());
-        orderRefund.setRefundSum(order.getOrgTotal());
-        orderRefund.setRefundFreight(new BigDecimal(0));
-        orderRefund.setFlowId(flowId);
-        orderRefund.setCreateTime(now);
-        orderRefund.setRefundDate(now);
-        orderRefund.setRefundStatus(SystemRefundPayStatusEnum.refundStatusIng.getType());//退款中
-        orderRefund.setRefundDesc(refundDesc);
         orderRefundMapper.save(orderRefund);
     }
 
