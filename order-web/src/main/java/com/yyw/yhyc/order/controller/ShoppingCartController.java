a9f62015c0d008dd238f5d21eff9c13245d4919d
lizhou <lizhou@111.com.cn>
2016-09-06 16:23:56 +0800

    1号药城三期：购物车页面商品处理-调用接口查询商品信息

--- order-web/src/main/java/com/yyw/yhyc/order/controller/ShoppingCartController.java
+++ order-web/src/main/java/com/yyw/yhyc/order/controller/ShoppingCartController.java
@@ -17,11 +17,14 @@ import com.yyw.yhyc.bo.Pagination;
 import com.yyw.yhyc.bo.RequestListModel;
 import com.yyw.yhyc.bo.RequestModel;
 import com.yyw.yhyc.controller.BaseJsonController;
+import com.yyw.yhyc.helper.UtilHelper;
 import com.yyw.yhyc.order.bo.ShoppingCart;
+import com.yyw.yhyc.order.dto.ShoppingCartDto;
 import com.yyw.yhyc.order.dto.ShoppingCartListDto;
 import com.yyw.yhyc.order.dto.UserDto;
 import com.yyw.yhyc.order.service.ShoppingCartService;
 import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,8 +44,8 @@ public class ShoppingCartController extends BaseJsonController {
 	@Autowired
 	private ShoppingCartService shoppingCartService;
 
-//	@Reference
-//	private IProductDubboManageService iProductDubboManageService;
+	@Reference
+	private IProductDubboManageService iProductDubboManageService;
 
 	/**
 	* 通过主键查询实体对象
@@ -112,28 +115,71 @@ public class ShoppingCartController extends BaseJsonController {
 		shoppingCart.setCustId(userDto.getCustId());
 		List<ShoppingCartListDto> allShoppingCart = shoppingCartService.listAllShoppingCart(shoppingCart);
 
-//		logger.info("购物车页面数据：\n" + JSONArray.fromObject(allShoppingCart).toString());
+		if(UtilHelper.isEmpty(allShoppingCart)){
+			model.addObject("allShoppingCart",allShoppingCart);
+			model.setViewName("shoppingCart/index");
+			return model;
+		}
 
-		/* 获取商品图片 */
-//		logger.info("购物车页面-获取商品图片：iProductDubboManageService = " + iProductDubboManageService);
-//		if(!UtilHelper.isEmpty(allShoppingCart) && !UtilHelper.isEmpty(iProductDubboManageService)){
-//			for(ShoppingCartListDto shoppingCartListDto : allShoppingCart){
-//				if(UtilHelper.isEmpty(shoppingCartListDto) || UtilHelper.isEmpty(shoppingCartListDto.getShoppingCartDtoList())){
-//					continue;
-//				}
-//				for(ShoppingCartDto shoppingCartDto : shoppingCartListDto.getShoppingCartDtoList()){
-//					if(UtilHelper.isEmpty(shoppingCartDto)) continue;
-//
-//					Map map = new HashMap();
-//					map.put("spu_code", shoppingCartDto.getSpuCode());
-//					map.put("type_id", "1");
-//					List list = iProductDubboManageService.selectByTypeIdAndSPUCode(map);
-//					//TODO
-//
-//				}
-//			}
-//		}
+		/* 处理商品信息： */
+		for(ShoppingCartListDto shoppingCartListDto : allShoppingCart){
+			if(UtilHelper.isEmpty(shoppingCartListDto) || UtilHelper.isEmpty(shoppingCartListDto.getShoppingCartDtoList())){
+				continue;
+			}
+			for(ShoppingCartDto shoppingCartDto : shoppingCartListDto.getShoppingCartDtoList()){
+				if(UtilHelper.isEmpty(shoppingCartDto)) continue;
 
+				if(UtilHelper.isEmpty(iProductDubboManageService)){
+					logger.error("购物车页面-查询商品信息失败,iProductDubboManageService = " + iProductDubboManageService);
+				}
+
+				/* 最小起批量、最小拆零包装   */
+				Map map = new HashMap();
+				map.put("spu_code", shoppingCartDto.getSpuCode());
+				map.put("seller_code", shoppingCartDto.getSupplyId());
+
+				String unit = "1";
+				int saleStart = 1;
+				int upStep = 1;
+				List productList = null;
+				try{
+					logger.info("购物车页面-查询商品信息,请求参数:" + map);
+					productList = iProductDubboManageService.selectProductBySPUCodeAndSellerCode(map);
+					logger.info("购物车页面-查询商品信息,响应参数:" + JSONArray.fromObject(productList));
+				}catch (Exception e){
+					logger.error("购物车页面-查询商品信息失败:" + e.getMessage());
+				}
+				if(UtilHelper.isEmpty(productList) || productList.size() != 1){
+					logger.error("购物车页面-查询的商品信息异常" );
+				}else{
+					JSONObject productJson = JSONObject.fromObject(productList.get(0));
+					unit = (String) productJson.get("unit");
+					saleStart = (int) productJson.get("minimum_packing");
+				}
+				shoppingCartDto.setUnit(unit); //最小拆零包装单位
+				shoppingCartDto.setSaleStart(saleStart);//起售量
+				shoppingCartDto.setUpStep(Integer.valueOf(unit)); //每次增加、减少的 递增数量
+
+				/* 商品图片 */
+				map.put("type_id", "1");
+				String filePath ="";
+				List picUrlList = null;
+				try{
+					logger.info("购物车页面-查询商品图片,请求参数:" + map);
+					picUrlList = iProductDubboManageService.selectByTypeIdAndSPUCode(map);
+					logger.info("购物车页面-查询商品图片,响应参数:" + JSONArray.fromObject(picUrlList));
+				}catch (Exception e){
+					logger.error("购物车页面-查询商品图片失败，" + e.getMessage() );
+				}
+				if(UtilHelper.isEmpty(picUrlList) || picUrlList.size() != 1){
+					logger.error("购物车页面-查询商品图片异常" );
+				}else{
+					JSONObject productJson = JSONObject.fromObject(picUrlList.get(0));
+					filePath = (String) productJson.get("file_path");
+				}
+				shoppingCartDto.setProductImageUrl(filePath); //商品图片
+			}
+		}
 
 		model.addObject("allShoppingCart",allShoppingCart);
 		model.setViewName("shoppingCart/index");
