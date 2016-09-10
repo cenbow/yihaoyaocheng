e439e5bc36428cb7819cdeb991fd381a5556e4d4
shiyongxi <shiyongxi@111.com.cn>
2016-09-10 10:50:52 +0800

    定时任务
    Change-Id: Ie6e6130de4e2104227c9b81b6ab678683d7ba683

--- order-job/src/main/java/com/yyw/yhyc/job/order/service/impl/OrderDoneForDeliveryJobServiceImpl.java
+++ order-job/src/main/java/com/yyw/yhyc/job/order/service/impl/OrderDoneForDeliveryJobServiceImpl.java
@@ -1,11 +1,8 @@
 package com.yyw.yhyc.job.order.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Reference;
-import com.gangling.scheduler.AbstractJob;
-import com.gangling.scheduler.ExecResult;
-import com.gangling.scheduler.JobExecContext;
 import com.yao.trade.interfaces.credit.interfaces.CreditDubboServiceInterface;
-import com.yyw.yhyc.job.order.service.OrderDoneForDeliveryJobService;
+import com.yyw.yhyc.job.order.support.AbstractJob;
 import com.yyw.yhyc.order.service.OrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,27 +12,21 @@ import org.springframework.stereotype.Service;
 /**
  * Created by shiyongxi on 2016/8/3.
  */
-@Service("orderDoneForDeliveryJobService")
-public class OrderDoneForDeliveryJobServiceImpl extends AbstractJob implements OrderDoneForDeliveryJobService {
+//@Service("orderDoneForDeliveryJobService")
+public class OrderDoneForDeliveryJobServiceImpl extends AbstractJob {
     private static final Logger logger = LoggerFactory.getLogger(OrderDoneForDeliveryJobServiceImpl.class);
 
     @Autowired
     private OrderService orderService;
     @Reference(timeout = 50000)
     private CreditDubboServiceInterface creditDubboService;
+
     /**
      * 系统自动确认收货
      * 订单发货后7个自然日后系统自动确认收货
      */
     @Override
-    protected ExecResult doTask(JobExecContext jobExecContext) {
-        try {
-            orderService.updateDoneOrderForDelivery(creditDubboService);
-            return new ExecResult(0, "succeed!");
-        }catch (Exception ex){
-            logger.error(ex.getMessage(), ex);
-
-            return new ExecResult(4, "failed!");
-        }
+    protected void doTask() throws Exception{
+        orderService.updateDoneOrderForDelivery(creditDubboService);
     }
 }
---
+++ order-job/src/main/java/com/yyw/yhyc/job/order/support/IJobService.java
@@ -0,0 +1,10 @@
+package com.yyw.yhyc.job.order.support;
+
+import com.gangling.scheduler.JobExecContext;
+
+/**
+ * Created by shiyongxi on 2016/9/9.
+ */
+public interface IJobService {
+    void execute();
+}
