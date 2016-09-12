9b2b9a339f9cf5e6b04405ff8386283fe0bf7411
liuyang <liuyang@111.com.cn>
2016-09-12 17:18:04 +0800

    ERP对接订单下发

---
+++ order-web/src/main/java/com/yyw/yhyc/order/controller/OrderIssuedController.java
@@ -0,0 +1,102 @@
+/**
+ *
+ * Created By: XI
+ * Created On: 2016-9-10 10:28:42
+ *
+ * Amendment History:
+ * 
+ * Amended By       Amended On      Amendment Description
+ * ------------     -----------     ---------------------------------------------
+ *
+ **/
+package com.yyw.yhyc.order.controller;
+
+import com.yyw.yhyc.order.bo.OrderIssued;
+import com.yyw.yhyc.order.service.OrderIssuedService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.yyw.yhyc.controller.BaseJsonController;
+import com.yyw.yhyc.bo.RequestListModel;
+import com.yyw.yhyc.bo.RequestModel;
+import com.yyw.yhyc.bo.Pagination;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Map;
+
+
+@Controller
+@RequestMapping(value = "/order/orderIssued")
+public class OrderIssuedController extends BaseJsonController{
+	private static final Logger logger = LoggerFactory.getLogger(OrderIssuedController.class);
+
+	@Autowired
+	private OrderIssuedService orderIssuedService;
+
+	/**
+	* 通过主键查询实体对象
+	* @return
+	*/
+	@RequestMapping(value = "/getByPK/{key}", method = RequestMethod.GET)
+	@ResponseBody
+	public OrderIssued getByPK(Integer key) throws Exception
+	{
+		return orderIssuedService.getByPK(key);
+	}
+
+	/**
+	* 分页查询记录
+	* @return
+	*/
+	@RequestMapping(value = {"", "/listPg"}, method = RequestMethod.GET)
+	@ResponseBody
+	public Pagination<OrderIssued> listPgOrderIssued(@RequestBody RequestModel<OrderIssued> requestModel) throws Exception
+	{
+		Pagination<OrderIssued> pagination = new Pagination<OrderIssued>();
+
+		pagination.setPaginationFlag(requestModel.isPaginationFlag());
+		pagination.setPageNo(requestModel.getPageNo());
+		pagination.setPageSize(requestModel.getPageSize());
+
+		return orderIssuedService.listPaginationByProperty(pagination, requestModel.getParam());
+	}
+
+	/**
+	* 新增记录
+	* @return
+	*/
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public void add(@RequestBody OrderIssued orderIssued) throws Exception
+	{
+		orderIssuedService.save(orderIssued);
+	}
+
+	/**
+	* 根据多条主键值删除记录
+	* @return
+	*/
+	@RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+	public void delete(@RequestBody RequestListModel<Integer> requestListModel) throws Exception
+	{
+		orderIssuedService.deleteByPKeys(requestListModel.getList());
+	}
+
+	/**
+	* 修改记录
+	* @return
+	*/
+	@RequestMapping(value = "/update", method = RequestMethod.PUT)
+	public void update(@RequestBody OrderIssued orderIssued) throws Exception
+	{
+		orderIssuedService.update(orderIssued);
+	}
+	@RequestMapping(value = "/findOrderIssuedListBySupplyId/{supplyId}", method = RequestMethod.GET)
+	@ResponseBody
+	public Map<String,Object> findOrderIssuedListBySupplyId(Integer supplyId) throws Exception{
+		return orderIssuedService.findOrderIssuedListBySupplyId(6067);
+	}
+}
