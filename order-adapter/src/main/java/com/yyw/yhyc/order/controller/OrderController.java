5aa189f907d07671df14a14f90a86b93e1a44d38
zhangqiang <936669533@qq.com>
2016-09-20 16:33:35 +0800

    APP接口开发

--- order-adapter/src/main/java/com/yyw/yhyc/order/controller/OrderController.java
+++ order-adapter/src/main/java/com/yyw/yhyc/order/controller/OrderController.java
@@ -12,20 +12,24 @@
 package com.yyw.yhyc.order.controller;
 
 import com.yyw.yhyc.controller.BaseJsonController;
+import com.yyw.yhyc.helper.UtilHelper;
 import com.yyw.yhyc.order.bo.Order;
 import com.yyw.yhyc.bo.Pagination;
 import com.yyw.yhyc.bo.RequestListModel;
 import com.yyw.yhyc.bo.RequestModel;
 import com.yyw.yhyc.order.dto.OrderDetailsDto;
+import com.yyw.yhyc.order.dto.OrderDto;
+import com.yyw.yhyc.order.dto.UserDto;
 import com.yyw.yhyc.order.service.OrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Controller
 @RequestMapping(value = "/order")
@@ -93,5 +97,123 @@ public class OrderController extends BaseJsonController {
 		orderService.update(order);
 	}
 
+	/**
+	 * 采购商取消订单
+	 * @return
+	 */
+	@RequestMapping(value = "/cancelOrder", method = RequestMethod.GET)
+	@ResponseBody
+	public Map<String,Object> cancelOrder(String orderId) throws Exception
+	{
+		UserDto userDto = super.getLoginUser();
+		if(UtilHelper.isEmpty(userDto))
+			userDto = new UserDto();userDto.setCustId(6066);
+		Map<String,Object> map = new HashMap<String,Object>();
+		int statusCode ;
+		String message = "";
+		Object data = null;
+		int SUCCESS = 0,EXCEPTION = -3,DATA_EXCEPTION = -1,TOEKN_EXCEPTION= -2;
+		try{
+			statusCode = SUCCESS;
+			message = "成功";
+			orderService.updateOrderStatusForBuyer(userDto, orderId);
+		}catch (Exception e){
+			statusCode = EXCEPTION;
+			message = e.getMessage();
+		}
+		map.put("statusCode",statusCode);
+		map.put("message",message);
+		map.put("data",data);
+		return map;
+	}
+
+
+	/**
+	 * 采购商取消订单信息
+	 * @return
+	 */
+	@RequestMapping(value = "/cancelOrderInfo", method = RequestMethod.GET)
+	@ResponseBody
+	public Map<String,Object> cancelOrderInfo(String orderId) throws Exception
+	{
+		UserDto userDto = super.getLoginUser();
+		if(UtilHelper.isEmpty(userDto))
+			userDto = new UserDto();userDto.setCustId(6066);
+		return convert(orderService.findOrderCancelInfo(orderId, userDto));
+	}
+
+
+	/**
+	 * 个人中心订单状态统计
+	 * @return
+	 */
+	@RequestMapping(value = "/getUserTipInfo", method = RequestMethod.GET)
+	@ResponseBody
+	public Map<String,Object> cancelOrderInfo() throws Exception
+	{
+		UserDto userDto = super.getLoginUser();
+		if(UtilHelper.isEmpty(userDto))
+			userDto = new UserDto();userDto.setCustId(6066);
+		return convert(orderService.listBuyerOrderStatusCount(userDto.getCustId()));
+	}
+
+	/**
+	 * 采购商获取订单列表
+	 * @return
+	 */
+	@RequestMapping(value = "/listOrder", method = RequestMethod.POST,consumes="application/json")
+	@ResponseBody
+	public Map<String,Object> listOrder(@RequestBody RequestModel<Map<String,String>> requestModel) throws Exception
+	{
+		Pagination<OrderDto> pagination = new Pagination<OrderDto>();
+		pagination.setPaginationFlag(requestModel.isPaginationFlag());
+		pagination.setPageNo(requestModel.getPageNo());
+		pagination.setPageSize(requestModel.getPageSize());
+		Map<String,String> param = requestModel.getParam();
+		UserDto userDto = super.getLoginUser();
+		if(UtilHelper.isEmpty(userDto))
+			userDto = new UserDto();userDto.setCustId(6066);
+		return convert(orderService.listBuyerOderForApp(pagination, param.get("orderStatus"),userDto.getCustId()));
+	}
+
+	/**
+	 * 采购商获取异常订单列表
+	 * @return
+	 */
+	@RequestMapping(value = "/exceptionOrder", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String,Object> exceptionOrder(@RequestBody RequestModel<Map<String,String>> requestModel) throws Exception
+	{
+		Pagination<OrderDto> pagination = new Pagination<OrderDto>();
+		pagination.setPaginationFlag(requestModel.isPaginationFlag());
+		pagination.setPageNo(requestModel.getPageNo());
+		pagination.setPageSize(requestModel.getPageSize());
+		Map<String,String> param = requestModel.getParam();
+		UserDto userDto = super.getLoginUser();
+		if(UtilHelper.isEmpty(userDto))
+			userDto = new UserDto();userDto.setCustId(6066);
+		return convert(orderService.listBuyerExceptionOderForApp(pagination, param.get("orderStatus"),userDto.getCustId()));
+	}
+
+
+	Map<String,Object> convert(Object object){
+		Map<String,Object> map = new HashMap<String,Object>();
+		int statusCode ;
+		String message = "";
+		Object data = null;
+		int SUCCESS = 0,EXCEPTION = -3,DATA_EXCEPTION = -1,TOEKN_EXCEPTION= -2;
+		try{
+			statusCode = SUCCESS;
+			message = "成功";
+			data = object;
+		}catch (Exception e){
+			statusCode = EXCEPTION;
+			message = e.getMessage();
+		}
+		map.put("statusCode",statusCode);
+		map.put("message",message);
+		map.put("data",data);
+		return map;
+	}
 
 }
