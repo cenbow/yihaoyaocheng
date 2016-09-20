42cb38d71f23fac34a3bb80f8038ad6124601cf1
shiyongxi <shiyongxi@111.com.cn>
2016-09-20 18:14:22 +0800

    获取api版本
    Change-Id: If185f696d67d413d06a6477c702291b9b1e09789

--- order-adapter/src/main/java/com/yyw/yhyc/order/controller/AppVersionController.java
+++ order-adapter/src/main/java/com/yyw/yhyc/order/controller/AppVersionController.java
@@ -6,7 +6,6 @@ import com.yyw.yhyc.order.service.AppVersionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
