681f00064a7255a1d7d4b49bbcebfc22560851d0
huyi <huyi@111.com.cn>
2016-10-31 10:53:39 +0800

    添加支付宝dubbo service类修改

--- order-service/src/main/java/com/yyw/yhyc/pay/impl/CmbPayServiceImpl.java
+++ order-service/src/main/java/com/yyw/yhyc/pay/impl/CmbPayServiceImpl.java
@@ -474,4 +474,14 @@ public class CmbPayServiceImpl implements PayService{
 
         return null;
     }
+
+    @Override
+    public String alipayCommit(String out_trade_no, String subject, String total_fee, String body) {
+        return null;
+    }
+
+    @Override
+    public String alipayrefundFastpayByMap(int batch_num, Map<Integer, String> refundMap) {
+        return null;
+    }
 }
