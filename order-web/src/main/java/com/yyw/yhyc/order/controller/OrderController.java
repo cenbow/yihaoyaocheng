005a80b1b0d1a988d505bc810a0e4493dfd6f602
wangkui01 <wangkui01@111.com.cn>
2017-01-05 15:57:43 +0800

    优化创建订单的逻辑

--- order-web/src/main/java/com/yyw/yhyc/order/controller/OrderController.java
+++ order-web/src/main/java/com/yyw/yhyc/order/controller/OrderController.java
@@ -14,7 +14,6 @@ import java.io.OutputStream;
 import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -45,7 +44,6 @@ import com.yao.trade.interfaces.credit.model.CreditDubboResult;
 import com.yao.trade.interfaces.credit.model.CreditParams;
 import com.yaoex.druggmp.dubbo.service.interfaces.IProductDubboManageService;
 import com.yaoex.druggmp.dubbo.service.interfaces.IPromotionDubboManageService;
-import com.yaoex.framework.core.model.util.StringUtil;
 import com.yaoex.usermanage.interfaces.adviser.IAdviserManageDubbo;
 import com.yaoex.usermanage.interfaces.custgroup.ICustgroupmanageDubbo;
 import com.yaoex.usermanage.model.adviser.AdviserModel;
@@ -67,21 +65,17 @@ import com.yyw.yhyc.order.dto.ShoppingCartDto;
 import com.yyw.yhyc.order.dto.ShoppingCartListDto;
 import com.yyw.yhyc.order.dto.UserDto;
 import com.yyw.yhyc.order.enmu.OnlinePayTypeEnum;
-import com.yyw.yhyc.order.enmu.OrderPayStatusEnum;
 import com.yyw.yhyc.order.enmu.SystemOrderStatusEnum;
 import com.yyw.yhyc.order.enmu.SystemPayTypeEnum;
 import com.yyw.yhyc.order.service.OrderCreateService;
 import com.yyw.yhyc.order.service.OrderExportService;
-
 import com.yyw.yhyc.order.service.OrderFullReductionService;
-
 import com.yyw.yhyc.order.service.OrderLogService;
-
 import com.yyw.yhyc.order.service.OrderService;
+import com.yyw.yhyc.order.service.OrderSubmitService;
 import com.yyw.yhyc.order.service.ShoppingCartService;
 import com.yyw.yhyc.order.service.SystemDateService;
 import com.yyw.yhyc.order.service.SystemPayTypeService;
-import com.yyw.yhyc.usermanage.bo.UsermanageEnterprise;
 import com.yyw.yhyc.usermanage.service.UsermanageEnterpriseService;
 import com.yyw.yhyc.utils.CacheUtil;
 import com.yyw.yhyc.utils.DateUtils;
@@ -137,6 +131,8 @@ public class OrderController extends BaseJsonController {
 
 	@Autowired
 	private OrderLogService orderLogService;
+	@Autowired
+	private OrderSubmitService orderSubmitService;
     /**
      * 通过主键查询实体对象
      * @return
@@ -219,119 +215,7 @@ public class OrderController extends BaseJsonController {
 		}else{
 			CacheUtil.getSingleton().add(createOrderUserCacheKey,createOrderUserCacheKey,3);
 		}
-
-		Map<String,Object> map = new HashMap<String, Object>();
-		for(OrderDto orderDto : orderCreateDto.getOrderDtoList()){
-			/* 校验采购商状态、资质 */
-			UsermanageEnterprise buyer = enterpriseService.getByEnterpriseId(userDto.getCustId()+"");
-			if(UtilHelper.isEmpty(buyer)){
-				logger.info("统一校验订单商品接口-buyer ：" + buyer);
-				map.put("result", false);
-				map.put("message", "采购商不存在");
-				map.put("goToShoppingCart", true);
-				return map;
-			}
-			orderDto.setBuyer(buyer);
-			orderDto.setCustId(Integer.valueOf(buyer.getEnterpriseId()));
-			orderDto.setCustName(buyer.getEnterpriseName());
-
-			/* 校验要供应商状态、资质 */
-			UsermanageEnterprise seller = enterpriseService.getByEnterpriseId(orderDto.getSupplyId() + "");
-			if(UtilHelper.isEmpty(seller)){
-				logger.info("统一校验订单商品接口-seller ：" + seller);
-				map.put("result", false);
-				map.put("message", "供应商不存在");
-				map.put("goToShoppingCart", true);
-				return map;
-			}
-			orderDto.setSeller(seller);
-			orderDto.setSupplyName(seller.getEnterpriseName());
-
-			/* 商品信息校验 ： 检验商品上架、下架状态、价格、库存、订单起售量等一系列信息 */
-			
-			/*map = orderService.validateProducts(userDto,orderDto,iCustgroupmanageDubbo,productDubboManageService,
-					productSearchInterface,iPromotionDubboManageService);*/
-			
-			map = this.orderCreateService.validateProducts(userDto,orderDto,iCustgroupmanageDubbo,productDubboManageService,
-			productSearchInterface,iPromotionDubboManageService);
-			boolean result = (boolean) map.get("result");
-			if(!result){
-				return map;
-			}
-
-			/**销售顾问信息**/
-			if(StringUtil.isNotEmpty(orderDto.getAdviserName())){
-				String [] adviserInfo = orderDto.getAdviserName().split(";");
-				orderDto.setAdviserCode(adviserInfo[0]);
-				orderDto.setAdviserName(adviserInfo[1]);
-				orderDto.setAdviserPhoneNumber(adviserInfo[2]);
-				if(adviserInfo.length > 3){
-					orderDto.setAdviserRemark(adviserInfo[3]);
-				}
-			}
-		}
-		//订单来源 限用pc
-		orderCreateDto.setSource(1);
-		Map<String,Object> newOrderMap = orderService.createOrder(orderCreateDto,iPromotionDubboManageService);
-		List<Order> orderList = (List<Order>) newOrderMap.get("orderNewList");
-
-		String orderIdStr = "";
-		if(!UtilHelper.isEmpty(orderList)){
-			for(Order order : orderList){
-				if(UtilHelper.isEmpty(order)) continue;
-				if(UtilHelper.isEmpty(orderIdStr)){
-					orderIdStr += order.getOrderId();
-				}else{
-					orderIdStr += ","+order.getOrderId();
-				}
-				
-				orderLogService.insertOrderLog(this.request,"1",userDto.getCustId(),order.getFlowId(),orderCreateDto.getSource());
-			}
-		}
-		map = new HashMap<String,Object>();
-		map.put("url","/order/createOrderSuccess?orderIds="+orderIdStr);
-
-		/* 生成账期订单后，调用接口更新资信可用额度 */
-		List<Order> orderNewList = (List<Order>) newOrderMap.get("orderNewList");
-		logger.info("创建订单接口-资信可用额度更新接口,creditDubboService = " + creditDubboService);
-		if (!UtilHelper.isEmpty(orderNewList) && !UtilHelper.isEmpty(creditDubboService)) {
-			CreditParams creditParams = null;
-			for(Order order : orderNewList){
-				if(UtilHelper.isEmpty(order)) continue;
-				if (!SystemPayTypeEnum.PayPeriodTerm.getPayType().equals(order.getPayTypeId())) {
-					continue;
-				}
-				creditParams = new CreditParams();
-				creditParams.setStatus("1");  //创建订单设置为1，收货时设置2，已还款设置4，（取消订单）已退款设置为5，创建退货订单设置为6
-				creditParams.setFlowId(order.getFlowId());//订单编号
-				creditParams.setOrderTotal(order.getOrgTotal());//订单优惠后的金额
-				creditParams.setBuyerCode(order.getCustId() + "");
-				creditParams.setBuyerName(order.getCustName());
-				creditParams.setSellerCode(order.getSupplyId() + "");
-				creditParams.setSellerName(order.getSupplyName());
-				creditParams.setPaymentDays(order.getPaymentTerm());//账期
-				CreditDubboResult creditDubboResult = null;
-				try{
-					logger.info("创建订单接口-生成账期订单后，调用接口更新资信可用额度,请求参数 creditParams= " + creditParams);
-					creditDubboResult = creditDubboService.updateCreditRecord(creditParams);
-					logger.info("创建订单接口-生成账期订单后，调用接口更新资信可用额度,响应参数creditDubboResult= " + creditDubboResult);
-
-					/* 账期订单信息发送成功后，更新该订单的支付状态与支付时间 */
-					if(!UtilHelper.isEmpty(creditDubboResult) && "1".equals(creditDubboResult.getIsSuccessful())){
-						order.setOrderStatus(SystemOrderStatusEnum.BuyerAlreadyPaid.getType());
-						order.setPayStatus(OrderPayStatusEnum.PAYED.getPayStatus());
-						order.setPayTime(systemDateService.getSystemDate());
-						update(order);
-						logger.info("创建订单接口-生成账期订单后，成功更新资信可用额度,更新订单信息，order=" + order);
-					}else{
-						logger.info("创建订单接口-生成账期订单后，更新资信可用额度失败..message" + (UtilHelper.isEmpty(creditDubboResult) ? "null" : creditDubboResult.getMessage()));
-					}
-				}catch (Exception e){
-					logger.error(e.getMessage());
-				}
-
-			}
-		}
+		Map<String,Object> map =this.orderSubmitService.createOrderSubmit(request, orderCreateDto, userDto, iCustgroupmanageDubbo, productDubboManageService, productSearchInterface, iPromotionDubboManageService, creditDubboService);
 		return map;
 	}
 
