7b1e7771e00e494b96621d0eb7c2b7e4538afafa
lvhongjie <lvhongjie@111.com.cn>
2017-01-06 15:32:29 +0800

    结算信息修改

--- order-service/src/main/java/com/yyw/yhyc/order/service/OrderService.java
+++ order-service/src/main/java/com/yyw/yhyc/order/service/OrderService.java
@@ -2806,7 +2806,6 @@ public class OrderService {
 
 			SystemPayType systemPayType = systemPayTypeMapper.getByPK(order.getPayTypeId());
 
-
 			//如果是银联在线支付，生成结算信息，类型为订单取消退款
 			if(OnlinePayTypeEnum.UnionPayB2C.getPayTypeId().equals(systemPayType.getPayTypeId())
 					||OnlinePayTypeEnum.UnionPayNoCard.getPayTypeId().equals(systemPayType.getPayTypeId())
@@ -2832,7 +2831,11 @@ public class OrderService {
 				orderSettlement.setSupplyId(order.getSupplyId());
 				orderSettlement.setRemark(cancelResult);
 				orderSettlement.setConfirmSettlement("0");//生成结算信息时都未结算
-				orderSettlement.setSettlementMoney(order.getOrgTotal());
+				if(!UtilHelper.isEmpty(order.getPreferentialCancelMoney())){
+					orderSettlement.setSettlementMoney(order.getOrgTotal().subtract(order.getPreferentialCancelMoney()));
+				}else{
+					orderSettlement.setSettlementMoney(order.getOrgTotal());
+				}
 				orderSettlementMapper.save(orderSettlement);
 			}
 			if(systemPayType.getPayType().equals(SystemPayTypeEnum.PayOnline.getPayType())) {
